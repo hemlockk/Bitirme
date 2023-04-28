0de4e74646af3cc31d65634f33f6a3c8ef29b46f
hemlockk <ali.yusuf01@hotmail.com>
2023-04-28 16:25:12 +0300

    End of phase 1

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ProductsController
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProduct()
+        {
+            return await _context.Products.ToListAsync();
+        }
+        [HttpGet("{id}")]
+         public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
